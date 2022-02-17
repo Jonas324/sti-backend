43436f75f838cdfe4e506761e69b1db37b10af7d
Jonas324 <jonaswadin24@gmail.com>
2022-02-17 10:02:02 +0100

    hello

--- app.js
+++ app.js
@@ -10,9 +10,28 @@ app.use('/healtcheck', require('./routes/healthcheck.routes'))
 app.use(express.urlencoded({ extended: true}));
 app.use(cors())
 
-app.get("/", (req, res)=>{
+app.get("/football", (req, res)=>{
     headers={"http_status":200, "cache-control": "no-cache"}
-    body={"status": "available"}
+    body=
+    [
+        {
+            "name": "AIK",
+            "points":9
+        },
+        {
+            "name":"DIF",
+            "points":6
+        },
+        {
+            "name":"BK",
+            "points":3
+        },
+        {
+            "name":"IF",
+            "points":1
+        }
+    ]
+    res.set('Content-Type', 'application/json')
     res.status(200).send(body)
 })
 
